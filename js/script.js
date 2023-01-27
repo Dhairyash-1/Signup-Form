7d6b7e46a5d202a5e8a62de3d484d6be351cb105
Dhairyash Gupta <101561153+Dhairyash-1@users.noreply.github.com>
2023-01-28 02:42:50 +0530

    js form validation done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,79 @@
-const emailField = document.getElementById("email");
-const submit = document.getElementById("submit");
-const emailerror = document.getElementById("email-error");
-const otherInuptField = document.querySelectorAll("#error");
-submit.addEventListener("click", () => {
-  event.preventDefault();
-  if (
-    !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
-  ) {
-    emailerror.classList.remove("hidden");
-  }
-  otherInuptField.forEach((valueField) => {
-    if (!valueField.value) {
-      valueField.classList.remove("hidden");
-    }
-  });
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
 });
+
+// checking the all input value on submit
+
+function checkInputs() {
+  // trim to remove whitespace
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "firstName cannot be blanked");
+  } else {
+    setSuccessFor(firstName);
+  }
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "lastName cannot be blanked");
+  } else {
+    setSuccessFor(lastName);
+  }
+  if (passwordValue === "") {
+    setErrorFor(password, "password cannot be blanked");
+  } else {
+    setSuccessFor(password);
+  }
+  if (emailValue === "") {
+    setErrorFor(email, "email cannot be blanked");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "email is not vaild");
+  } else {
+    setSuccessFor(email);
+  }
+}
+
+// showing the error
+function setErrorFor(input, msg) {
+  const formControl = input.parentElement;
+
+  const span = formControl.querySelector("span");
+  const error = formControl.querySelector(".error");
+
+  error.classList.remove("invisible");
+  span.classList.remove("invisible");
+  span.innerText = msg;
+  input.style.borderColor = "red";
+}
+
+// showing the success
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  const error = formControl.querySelector(".error");
+  const span = formControl.querySelector("span");
+
+  const success = formControl.querySelector(".success");
+  success.classList.remove("invisible");
+
+  input.style.borderColor = "green";
+  if (!error.classList.contains("invisible")) {
+    error.classList.add("invisible");
+    span.classList.add("invisible");
+  }
+}
+
+// email validation regex
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
